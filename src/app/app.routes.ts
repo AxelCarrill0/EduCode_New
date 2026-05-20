bdab8d5279f939e5f50cc4c6d90c6ea5b18ce9c3
AxelCarrill0 <axelcarrilloe28@gmail.com>
2026-05-20 15:04:39 -0500

    001_Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {HomeContentComponent} from './modules/home/components/home-content/home-content.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: HomeContentComponent}
+];
